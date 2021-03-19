c12d3f2a74bb7d195dd709f0207438d35b8d7913
Nile Walker <niwal7@morgan.edu>
2021-03-19 01:14:24 -0400

    Do you want rotation detection here

--- detect.py
+++ detect.py
@@ -1,27 +1,57 @@
 import cv2 
-import pyzbar
+from pyzbar.pyzbar import decode
 from tf import Pose
+from math import pi, acos
 
 def getGuideLinePosition(frame):
     pass 
 
-def checkForMarker(frame):
-	return None, None
-# barcodes = pyzbar.decode(frame)
+def sss(d,e,f):
+    """ This function solves the triangle and returns (d,e,f,D,E,F) """
+    assert d + e > f and e + f > d and f + d > e
+    F = acos((d**2 + e**2 - f**2) / (2 * d * e))
+    E = acos((d**2 + f**2 - e**2) / (2 * d * f))
+    D = pi - F - E
+    return (d,e,f,D,E,F)
 
-# # loop over the detected barcodes
-# for barcode in barcodes:
-# 	# extract the bounding box location of the barcode and draw
-# 	# the bounding box surrounding the barcode on the image
-# 	(x, y, w, h) = barcode.rect
-# 	cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-# 	# the barcode data is a bytes object so if we want to draw it
-# 	# on our output image we need to convert it to a string first
-# 	barcodeData = barcode.data.decode("utf-8")
-# 	barcodeType = barcode.type
-# 	# draw the barcode data and barcode type on the image
-# 	text = "{} ({})".format(barcodeData, barcodeType)
-# 	cv2.putText(frame, text, (x, y - 10),
-# 		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+def dist(x1,y1,x2,y2):
+	return ((x1-x2)**2 + (y1-y2)**2)**.5
+
+def checkForMarker(frame):
+	barcodes = decode(frame)
+	if len(barcodes) > 0:
+		marker = barcodes[0] # Select the first available marker
+		p1 = marker.polygon[0] # Calculate its rotation in the camera frame
+		p2 = marker.polygon[1]
+		a = dist(p1.x, p1.y, p2.x, p2.y) + .000001
+		b = dist(p2.x, p2.y, frame.shape[1], p2.y) + .000001
+		c = dist(p1.x, p1.y, frame.shape[1], p2.y) + .000001 
+		a,b,c, A,B,C = sss(a,b,c)
+		rot = C*(180/pi)
+		# Flip Quadrant
+		if p1.y > p2.y:
+			rot = 180 + (180 % rot)
+	
+		return marker.data, Pose(rot=rot)
+	else:
+		return None, None
 if __name__ == '__main__':
-	pass
+	from imutils.video import VideoStream
+	import imutils
+	import time
+	import cv2
+
+	# initialize the video stream, sensors, etc
+	print("[INFO] starting video stream...")
+	vs = VideoStream(0).start()
+	time.sleep(2.0)
+
+	while True: # loop over the frames from the video stream
+		frame = vs.read()
+		frame = imutils.resize(frame, width=400)
+		ID, pose = checkForMarker(frame)
+		if ID:
+			print(pose)
+		cv2.imshow("d",frame)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
+			break
