7d66f6e0d2b3e1e94369da3cbe57b00a192f596b
NWalker4483 <niwal7@morgan.edu>
2021-04-13 15:43:03 -0400

    .

--- run.py
+++ run.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 import paho.mqtt.client as mqtt
-from imutils.video import VideoStream
 import imutils
 import time
 
 from driver import Driver
+from detector import Detector
 from planner import Planner
 
 from tf import TFManager, Pose
 from detect import getGuideLinePosition, checkForMarker
 
 global_qr_poses = dict()
-
-planner = Planner()
 driver = Driver()
+detector = Detector()
+planner = Planner()
 
 test = True
 if test:
     qr_map_file = "qr_map.txt"
     # Reading the global poses of each QR code
     print("[INFO] loading global marker position..")
-    with open(qr_map_file, "r") as fh:
-        for line in fh:
+    with open(qr_map_file, "r") as file_handle:
+        for line in file_handle:
             ID, pose = line.split(',', 1)
             x, y, rot = [int(i) for i in pose.strip()[1:-1].split(',')]
             global_qr_poses[int(ID)] = Pose(x, y, rot)
@@ -49,8 +49,7 @@ def on_message(client, userdata, msg):
     elif msg.topic == goal_update_topic:
         print(msg.payload.decode())
         planner.set_goal(Pose())
-    
-    
+
 manager = TFManager()
 
 manager.create_frame(map_frame)
@@ -68,22 +67,19 @@ client.on_message = on_message
 
 # initialize the video stream, sensors, etc
 print("[INFO] starting video stream...")
-vs = VideoStream(0).start()
-time.sleep(2.0)
+
 
 try:
     while True:  # loop over the frames from the video stream
         manager.publish(client=client)
         client.loop(timeout=.1)
-        frame = vs.read()
-        frame = imutils.resize(frame, width=400)
-
+   
         manager.set_tf(driver.pose_estimate, odom_frame, base_frame)
 
         # Get the position rotation and ID for any markers within view
         # * The sensor readings are always in the base frame frame
-        ID, local_marker_pose = checkForMarker(frame)
-        if ID:
+        dist_sensor_states, ID, local_marker_pose = detector.update()
+        if local_marker_pose != None:
             # Convert the measurement to be within the map frame
             global_marker_pose_estimate = manager.get_tf(
                 map_frame, base_frame) + local_marker_pose
@@ -91,11 +87,10 @@ try:
                 global_qr_poses[ID] - global_marker_pose_estimate, map_frame, odom_frame)
 
         # Update the planner with the most current global position estimate
-        new_cmd = planner.update(frame, manager.get_tf(map_frame, base_frame))
+        new_cmd = planner.update(manager.get_tf(map_frame, base_frame))
         driver.update(new_cmd)
 except Exception as e:
     raise(e)
 finally:
     print("[INFO] cleaning up...")
     client.disconnect()
-    vs.stop()
