2cc86dbf1d39f42217359b33ab96910b70d94710
Nile Walker <nilezwalker@gmail.com>
2021-09-07 18:06:25 -0400

    x

--- detector.py
+++ detector.py
@@ -307,20 +307,8 @@ class Detector(Thread):
         self.state_info["last_gray"] = gray
     def largestForegoodObject(self):
         frame = self.low_res_view
-        #gray= cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         mask = cv2.inRange(frame, self.lower_green, self.upper_green)
-        #edges = cv2.Canny(gray, 50,200)
-
         contours, hierarchy= cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-
-        cv2.destroyAllWindows()
-
-        def get_contour_areas(contours):
-            all_areas= []
-            for cnt in contours:
-                area= cv2.contourArea(cnt)
-                all_areas.append(area)
-            return all_areas
         sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
         largest_item = sorted_contours[0]
         self.state_info["largest_contour"] = largest_item
