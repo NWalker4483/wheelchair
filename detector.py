90932229a0dfbaf0f12daa390c956cd66b76e58e
Nile Walker <nilezwalker@gmail.com>
2021-09-19 13:46:36 -0400

    Ready to claibrate

--- detector.py
+++ detector.py
@@ -15,7 +15,7 @@ def scale_from(x, y, res_1, res_2):
     return x, y
 
 class Detector(Thread):
-    def __init__(self, filename = None, debug=False):
+    def __init__(self, filename = None, threaded = False,debug=False):
         super(Detector, self).__init__()
         self.debug = debug
         self.state_info = dict()
@@ -27,6 +27,8 @@ class Detector(Thread):
         self.high_res_view = None
         self.low_res_view = None
 
+        self.view_area_shape = () # meters
+
         self.hl_ratio = 3 # Not yet used 
         self.filename = filename
         if self.filename == None:
@@ -34,6 +36,7 @@ class Detector(Thread):
             self.camera_stream.stream.camera.shutter_speed = 2000 # Drop shutter speed to reduce motion blur
             self.camera_stream.start()
         else:
+            self.file_fps = 30
             self.video_stream = cv2.VideoCapture(filename)
             
         time.sleep(2)  # Warm Up camera
@@ -66,7 +69,7 @@ class Detector(Thread):
                     qualityLevel = 0.3,
                     minDistance = 7,
                     blockSize = 7)
-
+        
         # TODO: Convert to Pose Object
         self.state_info["velocity"] = dict()
         self.state_info["velocity"]["sample_time"] = time.time()
@@ -74,7 +77,32 @@ class Detector(Thread):
         self.state_info["velocity"]["y"] = 0
         self.state_info["velocity"]["r"] = 0
 
-        self.update_freq = 10 
+        ####### Image Matching
+        # Camera internals
+        size = (1, 1)
+        focal_length = size[1]
+        center = (size[1]/2, size[0]/2)
+        self.camera_matrix = camera_matrix = np.array(
+                                [[focal_length, 0, center[0]],
+                                [0, focal_length, center[1]],
+                                [0, 0, 1]], dtype = "double")
+
+        print(f"Camera Matrix :\n {camera_matrix}")
+
+        self.dist_coeffs = np.zeros((4,1)) # Assuming no lens distortion
+
+        # FLANN parameters
+        # FLANN_INDEX_KDTREE = 0
+        FLANN_INDEX_LSH = 6
+        index_params = dict(algorithm = FLANN_INDEX_LSH, trees = 5)
+        search_params = dict(checks=50)   # or pass empty dictionary
+
+        self.flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        self.MIN_MATCHES = 50
+        self.orb = cv2.ORB_create(nfeatures=250)
+
+        self.update_freq = 15
         self.state_info["odom"] = dict()
         self.state_info["odom"]["sample_time"] = time.time()
         self.state_info["odom"]["x"] = 0
@@ -83,17 +111,21 @@ class Detector(Thread):
 
         ###### Sensor Fusion
         self.initialized = False
-        self.alpha = .2
+        self.alpha = .4
         self.tau = 1/20
 
         ###### MultiThreading
         self.daemeon = True
         self.__alive = True
-        # self.start()
+        if threaded:
+            self.start()
 
     def run(self):
-        while self.__alive:
-            self.update()
+        try:
+            while self.__alive:
+                self.update()
+        finally:
+            self.stop()
 
     def stop(self):
         self.__alive = False
@@ -108,19 +140,15 @@ class Detector(Thread):
         frame = self.high_res_view
   
         if "marker" in self.state_info:
-            polygon = self.state_info["marker"]["polygon"]
+            
             x, y, r = self.state_info["marker"]["x"], self.state_info["marker"]["y"], self.state_info["marker"]["r"]
             ID = self.state_info["marker"]["id"]
-            
+
+            colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 255)]
+            polygon = self.state_info["marker"]["polygon"]
             # Draw QR Code corners
-            frame = cv2.circle(
-                frame, (polygon[0].x, polygon[0].y), 9, (255, 0, 0), 5)
-            frame = cv2.circle(
-                frame, (polygon[1].x, polygon[1].y), 10, (0, 255, 0), 5)
-            frame = cv2.circle(
-                frame, (polygon[2].x, polygon[2].y), 11, (0, 0, 255), 5)
-            frame = cv2.circle(
-                frame, (polygon[3].x, polygon[3].y), 12, (255, 255, 255), 5)
+            for i, (point, color) in enumerate(zip(polygon, colors)):
+                frame = cv2.circle(frame, (point.x, point.y), 9 + i, color , 5 - i)
             frame = cv2.circle(frame, (int(x), int(y)), 6, (255, 255, 255), 2)
 
             x, y = int(x), int(y)
@@ -169,22 +197,6 @@ class Detector(Thread):
             cv2.circle(
                 frame,scale_from(*p0,res_1 = self.low_res_shape,res_2 =self.high_res_shape), 6, (255, 0, 0), 10)
         
-        if "largest_contour" in self.state_info:
-            c=self.state_info["largest_contour"]
-            cv2.drawContours(frame, c, -1, (255,0,0),10)
-            # determine the most extreme points along the contour
-            extLeft = tuple(c[c[:, :, 0].argmin()][0])
-            extRight = tuple(c[c[:, :, 0].argmax()][0])
-            extTop = tuple(c[c[:, :, 1].argmin()][0])
-            extBot = tuple(c[c[:, :, 1].argmax()][0])
-            l_c =  ((extRight[0] + extLeft[0])//2, extBot[1])
-            
-            frame = cv2.circle(frame, extLeft, 2, (255, 0, 255), 7)
-            frame = cv2.circle(frame, extRight, 2, (255, 0, 255), 7)
-            frame = cv2.circle(frame, extTop, 2, (255, 0, 255), 7)
-            frame = cv2.circle(frame, extBot, 2, (255, 0, 255), 7)
-            frame = cv2.circle(frame, l_c, 2, (255, 255, 255), 7)
-        
         if "odom" in self.state_info:
             pass
         
@@ -196,6 +208,22 @@ class Detector(Thread):
             p1 = rotate_about((vx, vy), p0, vr)
             p2 = (int(p1[0] * scaler), int(p1[1] * scaler))
             cv2.line(frame, p0, p2, (0,0,255), 4)
+        
+        if "good_matches" in self.state_info:
+            
+            # Need to draw only good matches, so create a mask
+            matchesMask = []
+
+            #matchesMask[i]=[1,0]
+
+            #matchesMask = [[0,0] for i in range(len(matches))]
+
+            draw_params = dict(matchColor = (0,255,0),
+                singlePointColor = (255,0,0),
+                matchesMask = matchesMask,
+                flags = 0)
+            
+            #display = cv2.drawMatchesKnn(last, kp1, frame, kp2, matches, None, **draw_params)
 
         # Draw Center Line
         frame = cv2.line(frame, (frame.shape[1]//2, 0), (frame.shape[1]//2, frame.shape[0]),(0,233,0),2)
@@ -288,7 +316,7 @@ class Detector(Thread):
                 track = np.array(track)
                 times = track[:,-1]
                 track = track[:,:2]
-                track -= self.state_info["true_center"] 
+                #track -= self.state_info["true_center"] 
 
                 time_delta = -1 * (times[0] - times[-1])
                 track_delta = track[0] - track[-1]
@@ -327,25 +355,54 @@ class Detector(Thread):
 
     def updateOdometry(self):
         curr_time = time.time()
-        d_t = curr_time - self.state_info["odom"]["sample_time"]  
-        if d_t >= (1 / self.update_freq): 
-            dx = self.state_info["velocity"]["x"] * d_t
-            dy = self.state_info["velocity"]["y"] * d_t
-            dr = self.state_info["velocity"]["r"] * d_t
-            self.state_info["odom"]["r"] += dr
-            tdx, tdy = rotate_about((dx, dy), (0, 0), self.state_info["odom"]["r"])
-            self.state_info["odom"]["x"] += tdx
-            self.state_info["odom"]["y"] += tdy
-            self.state_info["odom"]["sample_time"] = curr_time 
-
-    def largestForegoodObject(self):
+         
         frame = self.high_res_view
-        mask = cv2.inRange(frame, self.lower_green, self.upper_green)
-        contours, hierarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        sorted_contours= sorted(contours, key=cv2.contourArea, reverse= True)
-        largest_item = sorted_contours[0]
-        self.state_info["largest_contour"] = largest_item
-    
+        last_keypoints = self.state_info.get("last_keypoints")
+
+        if last_keypoints != None:
+            d_t = curr_time - self.state_info["odom"]["sample_time"] 
+            if d_t >= (1 / self.update_freq): 
+                good_matches = []
+                try:
+                    kp1, des1 = last_keypoints
+                    kp2, des2 = self.orb.detectAndCompute(frame, None)
+                    self.state_info["last_keypoints"] = (kp2, des2)
+
+                    matches = self.flann.knnMatch(des1, des2, k=2)
+                
+                    # ratio test as per Lowe's paper
+                    # TODO: I do not understand this value error but it keeps coming up
+                    for i, pair in enumerate(matches):
+                        try:
+                            m, n = pair
+                            if m.distance < 0.7 * n.distance:
+                                good_matches.append(m)
+                        except ValueError:
+                            #print("Match Failed", i)
+                            pass
+                    self.state_info["good_matches"] = good_matches 
+                except Exception as e:
+                    print(e)
+                
+                if len(good_matches) >= self.MIN_MATCHES:
+                    old_points = np.float32([kp1[m.queryIdx].pt for m in good_matches])
+                    curr_points = np.float32([kp2[m.trainIdx].pt for m in good_matches])
+
+                    old_points = np.pad(old_points, [(0,0),(0,1)]) # Add Z axis of 0
+                    (success, rotation_vector, translation_vector, inliers) = cv2.solvePnPRansac(old_points, curr_points, self.camera_matrix, self.dist_coeffs, flags = cv2.SOLVEPNP_ITERATIVE)
+                    if success:
+                        dx, dy, dz = translation_vector
+                        dr = rotation_vector[2][0]
+                        print(dx, dy, dz, *rotation_vector)
+
+                        self.state_info["odom"]["r"] += dr
+                        tdx, tdy = rotate_about((dx, dy), (0, 0), self.state_info["odom"]["r"])
+                        self.state_info["odom"]["x"] += tdx
+                        self.state_info["odom"]["y"] += tdy
+                        self.state_info["odom"]["sample_time"] = curr_time
+        else:
+            self.state_info["last_keypoints"] = (self.orb.detectAndCompute(frame, None))
+        
     def readCameraFeed(self):
         if self.filename == None:
             frame = self.camera_stream.read()
@@ -354,20 +411,19 @@ class Detector(Thread):
         self.raw_res_shape = frame.shape
         self.raw_res_view = frame
         
-        
         frame = imutils.resize(frame, width=400)
         self.high_res_shape = frame.shape
         self.high_res_view = frame
         
         frame = imutils.resize(frame, width=200)
         self.low_res_shape = frame.shape
-        self.state_info["true_center"] = (frame.shape[1]//2, frame.shape[0]//2)
         self.low_res_view = frame
     
     def updateFusedMeasurements(self):
         # Store whatever the most recent line detection is so that it's available between short gaps in detection
         if self.state_info.get("line"):
             self.state_info["line_last"] = self.state_info["line"]
+            self.state_info["line_last"]["odom"] = self.state_info["odom"]
 
         if not self.initialized:
             if self.state_info.get("line"):
@@ -381,10 +437,9 @@ class Detector(Thread):
         d_t = curr_time - self.state_info["line_fused"]["sample_time"]
         if d_t >= self.tau:
             # Update the predicted value
-            dx = self.state_info["velocity"]["x"] * d_t
-            dy = self.state_info["velocity"]["y"] * d_t
-            dr = self.state_info["velocity"]["r"] * d_t
-
+            dx = self.state_info["odom"]
+            dy = 0
+            dr = 0
             mf_1, bf_1 = self.state_info["line_fused"]["slope"], self.state_info["line_fused"]["bias"]
             box_shape = self.low_res_shape[:2]
             if self.swap_xy:
@@ -394,12 +449,13 @@ class Detector(Thread):
             # I think Because the camera is mounted in reverse the velocities Need to be negative
             # I don't know if this for sure but I'm making a 
             # guess based on how the prediction moves in the opposite direction of motion when I don't flip them
+            
+            # TODO: Check for or prevent nan predictions 
             mp, bp = predict_newline(mf_1, bf_1, -dx, -dy, dr, box_shape)
 
             m, b = self.state_info["line_last"]["slope"], self.state_info["line_last"]["bias"]
                 
             # Apply filtering
-            # TODO: Check or prevent nan predictions 
             mf = mp * self.alpha + m * (1 - self.alpha)
             bf = bp * self.alpha + b * (1 - self.alpha)
             
@@ -410,9 +466,11 @@ class Detector(Thread):
         self.readCameraFeed()
         self.checkForMarker()
         self.estimateLineForm()
-        self.estimateVelocities()
+        # self.estimateVelocities()
         self.updateOdometry()
-        self.updateFusedMeasurements()
+
+        # TODO: Update to use odometry values instead of velocity
+        #self.updateFusedMeasurements()
 
         if self.state_info.get("marker") and False: # Default to Absolute Measurement
             self.state_info["line"] = dict()
@@ -461,25 +519,26 @@ class Detector(Thread):
         barcodes = decode(frame)
         marker = next(filter(lambda code: code.type == "QRCODE", barcodes), None)
         if marker != None:
+            self.state_info["marker"] = dict()
+            self.state_info["marker"]["polygon"] = marker.polygon
             try:
-                self.state_info["marker"] = dict()
-                self.state_info["marker"]["polygon"] = marker.polygon
                 self.state_info["marker"]["id"] = int(marker.data)
             except ValueError as ve:
                 self.state_info["marker"]["id"] = 0
-                print("WARN: Invalid QR CODE READ: Defaulting to id = 0 for development")
+                # TODO: Swap in an actual logger
+                print("WARNING: Invalid QR CODE READ: Defaulting to id = 0 for development")
             
             p1 = marker.polygon[0] # Upper Left 
             p2 = marker.polygon[1] # Lower Left 
             p3 = marker.polygon[2] # Lower Right
             p4 = marker.polygon[3] # Upper Right
             
-            v1 = (p1[0]-p2[0], p1[1]-p2[1])
+            v1 = (p1[0] - p2[0], p1[1] - p2[1]) # midpoint(p1, p2)
   
             rot = angle_between(v1, [0,1])
             
             #TODO: Convert Q4 and Q3 into postive radian values
-            #TODO: Rewrite above TODO
+            #TODO: Rewrite above TODO Works but standardize Rotation Values
             if v1[0] < 0: # Q3 + Q4 
                 rot = -rot
 
@@ -489,13 +548,11 @@ class Detector(Thread):
             self.state_info["marker"]["r"] = rot
             
 if __name__ == '__main__':
-    det = Detector(debug = True)
+    det = Detector(filename=0,debug = True)
     try:
         while True:  # loop over the frames from the video stream
             s = time.time()
             det.update()
-            if det.state_info.get("line") != None:
-                print(det.state_info["line"])
             #print("FPS: ", 1.0 / (time.time() - s))
     except KeyboardInterrupt as e:
         print("Manual ShutOff")
--- test_tracking.py
+++ test_tracking.py
@@ -3,10 +3,11 @@ import cv2
 #TODO: Draw some representation of rotation
 from utils.math import rotate_about
 
-def main(detector, explore_area = (1500, 1500), sample_freq = 10):
+def main(detector, explore_area = (5000, 5000), sample_freq = 10, start_pose = None):
     detector.update_freq = sample_freq
     x, y = 0,0
-    p1 = (explore_area[0]//2, explore_area[0]//2)
+    start_pose = (explore_area[0]//2, explore_area[0]//2)
+    p1 = start_pose
     p2 = (p1[0], p1[1] + 100)
     frame = np.zeros((*explore_area, 3))
     while True: 
@@ -15,12 +16,12 @@ def main(detector, explore_area = (1500, 1500), sample_freq = 10):
         cv2.circle(frame, (int(x),int(y)), 8, (255, 0, 0), -1) 
         
         x, y = detector.state_info["odom"]["x"], detector.state_info["odom"]["y"]
-        x, y = x + (explore_area[0]//2), y+(explore_area[1]//2)
+        x, y = x + start_pose[0], y + start_pose[1]
         p1 = (x, y)
         p2 = (p1[0], p1[1] + 100)
         p2 = rotate_about(p2, p1, detector.state_info["odom"]["r"])
         p1, p2 = (int(p1[0]),int(p1[1])), (int(p2[0]),int(p2[1]))
-        cv2.line(frame, p1,p2, (0,0,255), 10)
+        cv2.line(frame, p1,p2, (0 ,0 ,255), 10)
         cv2.circle(frame, (int(x), int(y)), 8, (255, 255, 255), -1) 
         cv2.imshow("frame", cv2.resize(frame, (500,500)))
         cv2.waitKey(1)
